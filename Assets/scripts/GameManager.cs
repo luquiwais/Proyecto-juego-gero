7cd1d076376063922b95613a15b6162783af37db
luquiwais <luquiwais@gmail.com>
2026-05-29 11:02:36 -0300

    avance un poco mas

--- Assets/scripts/GameManager.cs
+++ Assets/scripts/GameManager.cs
@@ -1,18 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    // Variable entera para llevar la cuenta de los objetos recolectados
+    private int objetosRecolectados = 0;
 
-    // Update is called once per frame
-    void Update()
+    // Este método se ejecuta automáticamente cuando otro objeto entra en el Trigger
+    private void OnTriggerEnter(Collider other)
     {
-        
+        // Comprobamos si el objeto con el que chocamos tiene la etiqueta "Coleccionable"
+        if (other.CompareTag("Coleccionable"))
+        {
+            // Sumamos 1 al contador
+            objetosRecolectados++;
+
+            // Mostramos el total actual en la consola de Unity
+            Debug.Log("¡Objeto recolectado! Total: " + objetosRecolectados);
+
+            // Destruimos el objeto con el que se ha colisionado (la taza)
+            Destroy(other.gameObject);
+        }
     }
 }
